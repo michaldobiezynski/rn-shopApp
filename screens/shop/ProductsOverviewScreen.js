ba168b4f95c9f0ca318006872ab6f9afaea387ca
michaldobiezynski <dobiezynski@gmail.com>
2020-05-31 20:09:00 +0100

    The products overview screen and redux and dummy data

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+const ProductsOverviewScreen = (props) => {
+  return <FlatList></FlatList>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductsOverviewScreen;
